0160f15480179665cb40cd089965bbb5b7e9067b
rgardishyan <rgardishyan@gmail.com>
2018-02-01 12:21:26 -0500

    Something

--- notatetr/models.py
+++ notatetr/models.py
@@ -2,10 +2,13 @@ from django.db import models
 import random
 
 def generate_id():
-  return ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+  return str(random.randint(100000, 999999999))
 
 # Create your models here.
 class User(models.Model):
   email = models.EmailField(max_length=254)
   password = models.CharField(max_length=50)
-  slug = models.CharField(default=generate_id)
+  slug = models.CharField(max_length=10, default=generate_id)
+  
+  def __str__(self):
+      return self.email
--- notatetr/views.py
+++ notatetr/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . import models
 # Create your views here.
 from django.template import loader
 from django.http import HttpResponse
@@ -7,7 +7,19 @@ from django.http import HttpResponse
 # Create your tests here.
 def login(request):
     if request.method == "POST":
-      pass
+      try:
+        user = models.User.objects.get(email=request.POST['email'], password=request.POST['password'])
+        request.session["notatetr_user_id"] = user.slug
+        request.session["notatetr_logged_in"] = True
+        return redirect("/")
+      except Exception as e:
+        return HttpResponse("No User: {}".format(e))
     else:
       template = loader.get_template("notebook/login-register.html")
       return HttpResponse(template.render(request=request, context={}))
+
+def index(request):
+    if request.session["notatetr_logged_in"]:
+      return HttpResponse(request.session["notatetr_user_id"])
+    else:
+      return HttpResponse(index)
