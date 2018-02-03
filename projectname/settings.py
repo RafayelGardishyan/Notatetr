0b3f1a13c6c2f4e2761ca95c43da502554ef6f6b
RafayelGardishyan <rgardishyan@gmail.com>
2018-02-03 13:53:12 +0100

    Update settings.py

--- projectname/settings.py
+++ projectname/settings.py
@@ -53,6 +53,13 @@ INSTALLED_APPS = [
 #     )
 # }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
+
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.security.SecurityMiddleware',
